c9a73557770503c17f8769099d86e0ec35a9e9af
Mario Vasilev <66969704+mariovas3@users.noreply.github.com>
2024-01-15 12:24:59 +0000

    Add optimistic init (#2)
    * made bandit experiments a bit more modular
    
    * move metric tracker to utils.py
    
    * add optimistic initial value bandits
    
    * update pre-commit hooks
    
    * update pre commit hooks black args
    
    * add reference to code in bandits.md

--- src/bandits/optimistic_init.py
+++ src/bandits/optimistic_init.py
@@ -9,7 +9,13 @@ from utils import (AvgRewardAndTrueOptTracker, EpsGreedyEnv,
                    get_normal_bandits, run_algo)
 
 
-def plot_testbed(file_name, algos, avg_rewards, prop_true_optimal):
+def plot_testbed(
+    file_name,
+    algos,
+    avg_rewards,
+    prop_true_optimal,
+    init_vals,
+):
     fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(7, 10))
     axs[0].set_ylabel("avg reward")
     axs[0].set_xlabel("Steps")
@@ -19,12 +25,18 @@ def plot_testbed(file_name, algos, avg_rewards, prop_true_optimal):
         axs[0].plot(
             range(avg_rewards.shape[-1]),
             avg_rewards[idx, :],
-            label=f"$\epsilon$={1-algo.probs.item():.2f}",
+            label=(
+                f"$\epsilon$={1-algo.probs.item():.2f}, "
+                f"$Q_0$={init_vals[idx]:.2f}"
+            ),
         )
         axs[1].plot(
             range(prop_true_optimal.shape[-1]),
             prop_true_optimal[idx, :],
-            label=f"$\epsilon$={1-algo.probs.item():.2f}",
+            label=(
+                f"$\epsilon$={1-algo.probs.item():.2f}, "
+                f"$Q_0$={init_vals[idx]:.2f}"
+            ),
         )
         axs[0].legend()
         axs[1].legend()
@@ -42,7 +54,7 @@ if __name__ == "__main__":
     if not SAVE_PATH.exists():
         SAVE_PATH.mkdir(parents=True)
 
-    file_name = SAVE_PATH / "ten-arm-testbed.png"
+    file_name = SAVE_PATH / "optimistic_init.png"
 
     # get 2000 bandit problems with 10 arms;
     bandits = get_normal_bandits(NUM_BANDITS, NUM_ARMS)
@@ -51,7 +63,7 @@ if __name__ == "__main__":
     true_optimal = bandits.mean.argmax(-1).numpy()
 
     # eps = .1, .01, 0;
-    algos = [dists.Bernoulli(probs=p) for p in (0.9, 0.99, 1)]
+    algos = [dists.Bernoulli(probs=p) for p in (0.9, 1)]
 
     # init metric tracker;
     metric_tracker = AvgRewardAndTrueOptTracker(
@@ -61,8 +73,8 @@ if __name__ == "__main__":
     # run experiments;
     for idx, algo in enumerate(algos):
         # init action values at zero;
-        action_values = np.zeros((NUM_BANDITS, NUM_ARMS))
-        bandit_env = EpsGreedyEnv(algo, action_values)
+        action_values = np.zeros((NUM_BANDITS, NUM_ARMS)) + idx * 5
+        bandit_env = EpsGreedyEnv(algo, action_values, lr=0.1)
         now = time.time()
         run_algo(idx, bandits, bandit_env, NUM_STEPS, metric_tracker)
         print(f"run {idx} took: {time.time() - now:.2f} secs")
@@ -73,4 +85,5 @@ if __name__ == "__main__":
         algos,
         metric_tracker.avg_rewards,
         metric_tracker.prop_true_optimal,
+        init_vals=(0, 5),
     )
---
+++ src/bandits/utils.py
@@ -0,0 +1,103 @@
+from typing import Tuple
+
+import numpy as np
+import torch
+import torch.distributions as dists
+
+
+class AvgRewardAndTrueOptTracker:
+    def __init__(self, algos, num_steps, true_optimal):
+        self.algos = algos
+        self.num_steps = num_steps
+        self.avg_rewards = np.zeros((len(algos), num_steps))
+        self.prop_true_optimal = np.zeros((len(algos), num_steps))
+        self.true_optimal = true_optimal
+
+    def __call__(self, rewards, msk, idx, t):
+        # get avg reward for this time step across bandits;
+        self.avg_rewards[idx, t] = rewards.mean()
+
+        # get avg time optimal was selected;
+        self.prop_true_optimal[idx, t] = (msk == self.true_optimal).mean()
+
+
+class MyNormal(dists.Normal):
+    def __getitem__(self, idx):
+        return dists.Normal(loc=self.mean[idx], scale=self.scale[idx])
+
+
+def get_normal_bandits(num_bandits, num_arms):
+    return MyNormal(
+        loc=dists.Normal(0.0, 1.0).sample(
+            sample_shape=(num_bandits, num_arms)
+        ),
+        scale=1.0,
+    )
+
+
+class EpsGreedyEnv:
+    def __init__(self, algo, action_values, lr=None):
+        self.action_values = action_values
+        self.lr = lr
+        if lr is None:
+            self.pick_count = np.zeros_like(action_values)
+        self.algo = algo
+        self.num_bandits, self.num_arms = self.action_values.shape
+        self.temp = np.arange(len(action_values))
+
+    def __call__(self, bandits) -> Tuple[np.ndarray]:
+        """Get rewards and indexes of chosen arms."""
+        # see which bandits will be greedy
+        is_greedy = (
+            self.algo.sample(sample_shape=(self.num_bandits,)).int().numpy()
+        )
+
+        # presample 2000 random action indexes;
+        randoms = np.random.randint(
+            0, self.num_arms, size=(self.num_bandits,)
+        )
+
+        # get indexes of greedy with random tie breaking;
+        greedies = (
+            dists.Categorical(
+                probs=torch.from_numpy(
+                    self.action_values
+                    == self.action_values.max(-1, keepdims=True)
+                )
+            )
+            .sample()
+            .int()
+            .numpy()
+        )
+
+        # see which actions to play;
+        msk = is_greedy * greedies + (1 - is_greedy) * randoms
+
+        # sample rewards
+        rewards = bandits[self.temp, msk].sample().numpy()
+
+        if self.lr is None:
+            # update pick count;
+            self.pick_count[self.temp, msk] += 1
+
+            # update action values;
+            self.action_values[self.temp, msk] += (
+                rewards - self.action_values[self.temp, msk]
+            ) / self.pick_count[self.temp, msk]
+
+        else:
+            self.action_values[self.temp, msk] += (
+                rewards - self.action_values[self.temp, msk]
+            ) * self.lr
+
+        return rewards, msk
+
+
+def run_algo(idx, bandits, bandit_env, num_steps, metric_tracker):
+    for t in range(num_steps):
+        # get rewards and which arms got picked
+        # also update action value estimates;
+        rewards, msk = bandit_env(bandits)
+
+        # track metrics;
+        metric_tracker(rewards, msk, idx, t)
